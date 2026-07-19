0b11e9ee6d548d74347aa76a7ccbb3d3aa847a9c
jing <jingwy1026@gmail.com>
2026-07-19 20:20:23 +0800

    纠正错误，实现正确的结果

--- src/components/CardForm.vue
+++ src/components/CardForm.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="card-form">
         <label for="title">标题：</label>
-        <input type="text" v-model="childForm.value.title" placeholder="请输入卡片标题">
+        <input type="text" v-bind="props.modelValue.title" @input="handleInput('title', $event)" placeholder="请输入卡片标题">
         <label for="value">数值：</label>
-        <input type="text" v-model="childForm.value.value" placeholder="请输入卡片数值">
+        <input type="text" v-bind="props.modelValue.value" @input="handleInput('value', $event)" placeholder="请输入卡片数值">
         <label for="desc">说明：</label>
-        <input type="text" v-model="childForm.value.desc" placeholder="请输入卡片说明">
+        <input type="text" v-bind="props.modelValue.desc" @input="handleInput('desc', $event)" placeholder="请输入卡片说明">
         <a-button type="primary" @click="handleSubmit" size="small" style="margin-left: 10px;">新增</a-button>
     </div>
 </template>
@@ -23,38 +23,20 @@ const props = defineProps({
 const emit = defineEmits(['update:modelValue', 'submit'])
 
 
-const childForm = ref({
-    title: '',
-    value: '',
-    desc: ''
-})
-
 const handleSubmit = () => {
-    if (!childForm.value.title.trim()) {
-        console.log('请输入内容')
-        return
-    }
-    emit('submit', {
-        ...props.modelValue,
-        title: childForm.value.title,
-        value: childForm.value.value,
-        desc: childForm.value.desc
-    })
-
-    clearCardForm()
-
+    emit('submit')
 }
 
-// const handleTitleInput =(event)=> {
-//     // 获取到当前输入的最新值
-//     const newTitle = event.target.value
+const handleInput =(field, event)=> {
+    // 获取到当前输入的最新值
+    const newFieldValue = event.target.value
 
-//     // 通知父组件，title发生变化
-//     emit('update:modelValue', {
-//         ...props.cardForm,
-//         title: newTitle
-//     })
-// }
+    // 通知父组件，title发生变化
+    emit('update:modelValue', {
+        ...props.modelValue,
+        [field]: newFieldValue
+    })
+}
 </script>
 
 <style lang="css" scoped></style>
